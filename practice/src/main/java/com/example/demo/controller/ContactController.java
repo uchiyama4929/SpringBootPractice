ed99226ce14876a3ecf41d95bcaa64fa517909d6
tatsuki-uchiyama <fukunaga.kinoko4929@gmail.com>
2024-04-30 13:29:59 +0900

    お問い合わせ機能実装 お問い合わせの登録機能の実装

--- practice/src/main/java/com/example/demo/controller/ContactController.java
+++ practice/src/main/java/com/example/demo/controller/ContactController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,13 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.entity.Contact;
 import com.example.demo.form.ContactForm;
+import com.example.demo.repository.ContactRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ContactController {
+    @Autowired
+    private ContactRepository contactRepository;
 
 	@GetMapping("/contact")
 	public String contact(Model model) {
@@ -43,9 +48,46 @@ public class ContactController {
 	public String confirm(Model model, HttpServletRequest request) {
 		HttpSession session = request.getSession();
 
-		ContactForm contactForm = (ContactForm) session.getAttribute("contactForm");
-		model.addAttribute("contactForm", contactForm);
-		System.out.println(model);
-		return "confirmation";
-	}
+        ContactForm contactForm = (ContactForm) session.getAttribute("contactForm");
+        model.addAttribute("contactForm", contactForm);
+        return "confirmation";
+    }
+
+    @PostMapping("/contact/register")
+    public String register(Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        ContactForm contactForm = (ContactForm) session.getAttribute("contactForm");
+
+        Contact contact = new Contact();
+        contact.setLastName(contactForm.getLastName());
+        contact.setFirstName(contactForm.getFirstName());
+        contact.setEmail(contactForm.getEmail());
+        contact.setPhone(contactForm.getPhone());
+        contact.setZipCode(contactForm.getZipCode());
+        contact.setAddress(contactForm.getAddress());
+        contact.setBuildingName(contactForm.getBuildingName());
+        contact.setContactType(contactForm.getContactType());
+        contact.setBody(contactForm.getBody());
+
+        contactRepository.save(contact);
+
+        return "redirect:/contact/complete";
+    }
+
+    @GetMapping("/contact/complete")
+    public String complete(Model model, HttpServletRequest request) {
+
+        if (request.getSession(false) == null) {
+          return "redirect:/contact";
+        }
+
+        HttpSession session = request.getSession();
+        ContactForm contactForm = (ContactForm) session.getAttribute("contactForm");
+        model.addAttribute("contactForm", contactForm);
+
+        session.invalidate();
+
+        return "completion";
+    }
 }
